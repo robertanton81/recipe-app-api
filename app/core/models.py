b76b3bcb0741a721624820b9d850c8ff7bbb2246
Robert Anton <18146018+robertanton81@users.noreply.github.com>
2021-05-16 17:50:10 +0200

    superuser added

--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('email not provided')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,5 +15,20 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_normalized_email(self):
-        email = 'test@test.com'
-        user = get_user_model().objects.create_user(email)
+        email = 'test@TEST.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_email_invalid(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None)
+
+    def test_super_user_created(self):
+        user = get_user_model().objects.create_superuser(
+            'mr.rob@emai.com',
+            'pass1243'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
